f6dea44de906724c0c533f50f4dd09a61e3d1478
Vitor Tatsumi <108268622+VitorTatsumi@users.noreply.github.com>
2023-11-23 15:48:48 -0300

    Update main.py
    Comentado

--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 #### Nomes em inglês. Ex: Flying Pikachu ao invés de Pikachu Voador                                                     ####    
 #### Não faz busca por raridades acima de UR, nem por cartas PROMO                                                      ####
 #### Não faz busca por nenhum tipo de carta além de Pokemons                                                            ####
-#### Pokemons EX antigos deverão ser inseridos com -EX e Pokemons ex novos deverão ser inseridos sem o                ############################################################################################################################
+#### Pokemons EX antigos deverão ser inseridos com -EX e Pokemons ex novos deverão ser inseridos sem o    ############################################################################################################################
 
 
 ############################################################################################################################
@@ -31,7 +31,7 @@ from selenium.webdriver.common.keys import Keys
 from tkinter import *
 from tkinter import ttk
 
-
+##################################### Início de c
 #Criação da janela
 tela = Tk()
 #Edição do título
@@ -53,15 +53,9 @@ ttk.Button(frm, text="Quit", command=tela.destroy).grid(column=1, row=0)
 
 
 
-#Lendo a tabela
+#Lendo o arquivo Excel 
 tabela = pd.read_excel('BotTesteLiga\pteste.xlsx')
 df = pd.DataFrame(tabela, columns=['Nome','Código', 'Coleção', 'Menor', 'Medio', 'Maximo'])
-
-#Exibindo o conteúdo das cédulas 1 das colunas parametrizadas
-print(tabela['Nome'].values[1] + ' ' + tabela['Código'].values[1])
-#var = 'https://www.ligapokemon.com.br/?view=cards/card&card=' + tabela['Nome'].values[1]
-
-#print('https://www.ligapokemon.com.br/?view=cards/card&card=' + str(tabela['Nome'].values[1]) + '%20(' + str(tabela['Código'].values[1][1:4]) + '/'+ str(tabela['Código'].values[1][5:7]) + ')&ed=' + str(tabela['Coleção'].values[1]) + '&num=' + str(tabela['Código'].values[1][1:4]))
 
 #Quantidade de cartas 
 n = int(input('Qual a quantidade de cartas para consulta?\n '))
@@ -69,23 +63,20 @@ n = int(input('Qual a quantidade de cartas para consulta?\n '))
 #Abre o navegador Chrome atribuindo-o à variável
 navegador = webdriver.Chrome()
 
-
+#Função para busca e armazenamento das cartas na tabela
 def searchCard(qtd):
+    #Laço de repetição para verificação da quantidade de cartas previamente inserida
     for i in range(qtd):
-        #if (tabela['Nome'].values[1][-2:] == 'ex'):
-        #navegador.get('https://www.ligapokemon.com.br/?view=cards/card&card=' + str(tabela['Nome'].values[i]) + '%20(' + str(tabela['Código'].values[i][1:4]) + '/'+ str(tabela['Código'].values[i][5:7]) + ')&ed=' + str(tabela['Coleção'].values[i]) + '&num=' + str(tabela['Código'].values[i]))
 
+        #O navegador faz a busca diretamente pela URL, com os parâmetros das cartas (Nome, código, coleção, código)
         navegador.get('https://www.ligapokemon.com.br/?view=cards/card&card=' + str(tabela['Nome'].values[i]) + '%20' + str(tabela['Código'].values[i]) + '&ed=' + str(tabela['Coleção'].values[i]) + '&num=' + str(tabela['Código'].values[i]))
-        #https://www.ligapokemon.com.br/?view=cards/card&card=Kindler%20(179/172)&ed=BRS&num=179
-        #https://www.ligapokemon.com.br/?view=cards/card&card=Hoothoot%20(TG12/TG30)&ed=ARTG&num=TG12
-        #else:
-            #navegador.get('https://www.ligapokemon.com.br/?view=cards/card&card=' + str(tabela['Nome'].values[1]) + '%20(' + str(tabela['Código'].values[1][1:4]) + '/'+ str(tabela['Código'].values[1][5:7]) + ')&ed=' + str(tabela['Coleção'].values[1]) + '&num=' + str(tabela['Código'].values[1]))
-            #navegador.get('https://www.ligapokemon.com.br/?view=cards/card&card=' + str(tabela['Nome'].values[2]) + '%20(' + str(tabela['Código'].values[2][1:4]) + '/'+ str(tabela['Código'].values[2][5:7]) + ')&ed=' + str(tabela['Coleção'].values[2]) + '&num=' + str(tabela['Código'].values[2]))
+        #TryCatch para que o programa não seja fechado ao encontrar um erro, mas sim que exiba uma mensagem informando
         try:
+            #Através do elemento XPATH, o navegador encontra o objeto de texto e seu conteúdo. Neste caso, os valores das cartas, mínimo, médio e máximo.
             minVal = navegador.find_element('xpath', '/html/body/main/div[4]/div[1]/div/div[3]/div[2]/div/div[2]').get_attribute("textContent")
             midVal = navegador.find_element('xpath', '/html/body/main/div[4]/div[1]/div/div[3]/div[2]/div/div[4]').get_attribute("textContent")
             maxVal = navegador.find_element('xpath', '/html/body/main/div[4]/div[1]/div/div[3]/div[2]/div/div[6]').get_attribute("textContent")
-            #print('Menor valor: ' + minVal + '\nValor médio: ' + midVal + '\nMaior Valor: ' + maxVal)
+            #Pandas localiza a célula correta à se fazer atribuição do valor das cartas. Neste caso, pegando os valores de nome, código e coleção da carta atual e comparando ela com os valores dentro das variáveis 
             tabela.loc[tabela['Nome'] + tabela['Código'] + tabela['Coleção'] == str(tabela['Nome'].values[i]) + str(tabela['Código'].values[i]) + str(tabela['Coleção'].values[i]) , 'Menor']  = str(minVal)
             tabela.loc[tabela['Nome'] + tabela['Código'] + tabela['Coleção'] == str(tabela['Nome'].values[i]) + str(tabela['Código'].values[i]) + str(tabela['Coleção'].values[i]), 'Medio'] = str(midVal)
             tabela.loc[tabela['Nome'] + tabela['Código'] + tabela['Coleção'] == str(tabela['Nome'].values[i]) + str(tabela['Código'].values[i]) + str(tabela['Coleção'].values[i]), 'Maximo'] = str(maxVal)
@@ -94,7 +85,7 @@ def searchCard(qtd):
     return time.sleep(1)
 
 
-
+#Chama a função que busca e armazena a carta na tabela
 searchCard(n)
 navegador.quit()
 print('Busca realizada.\nDados salvos em ptesteNovo.xlsx')
